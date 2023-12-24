af4cdab1b52cf02aaaa230500aef6697c8fe0ec7
Mahavir-2003 <patelmahavir2003@outlook.com>
2023-12-24 16:04:44 +0530

    Project Initialization

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Oxion',
+  description: 'Welcome to Oxion, your AI ally for limitless possibilities. Experience the future of personalized interaction, creative content generation, and streamlined assistance. Unleash the power of Oxion – where innovation meets simplicity, enriching your digital journey like never before.',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from '@/components/ui/button'
+
+export default function Home() {
+  return (<>
+    <div>Hello shadcn</div>
+    <Button variant='destructive'>Hello There</Button>
+    </>
+  )
+}
